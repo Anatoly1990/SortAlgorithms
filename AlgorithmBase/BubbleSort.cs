509ed73b24823445c49508b15da0dadbf9705f1e
Anatoliy Novikov <chamskra66er@gmail.com>
2019-09-07 13:20:17 +0300

    Реализация пузырковой сортировки

---
+++ AlgorithmBase/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm;
+
+namespace AlgorithmBase
+{
+    public class BubbleSort<T>:AlgorithmBase<T> where T : IComparable
+    {
+        public override void Sort()
+        {
+            var count = Items.Count;
+            int i, j;
+            for (j = 0; j < count; j++)
+            {
+                for (i = 0; i < count - 1-j; i++)
+                {
+                    var a = Items[i];
+                    var b = Items[i + 1];
+                    if (a.CompareTo(b) == 1)
+                    {
+                        Swap(i, i + 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmTests1/BubbleSortTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmBase.Tests
+{
+    [TestClass()]
+    public class BubbleSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            //arrenge
+            var bubble = new BubbleSort<int>();          
+            var rnd = new Random();
+            var items = new List<int>();
+            for (int i = 0; i < 15; i++)
+            {
+                items.Add(rnd.Next(0,100));     
+            }
+            bubble.Items.AddRange(items);
+            items.Sort();//стадартная сортировка List
+            //act
+            bubble.Sort();
+            //assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(items[i],bubble.Items[i]);
+            }           
+
+        }
+    }
+}
