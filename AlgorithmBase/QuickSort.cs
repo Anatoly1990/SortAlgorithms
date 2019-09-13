2c8bb776c194079aa7c0b81de3c7f44f534ae5d1
Anatoliy Novikov <chamskra66er@gmail.com>
2019-09-13 13:47:46 +0300

    Реализация быстрой сортировки

---
+++ AlgorithmBase/QuickSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm;
+
+namespace AlgorithmBase
+{
+    public class QuickSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        public QuickSort(IComparable<T> items) { }
+        public QuickSort() { }
+        public override void Sort()
+        {
+            Qsort(0, Items.Count - 1);
+        }
+        private void Qsort(int left, int right)
+        {
+            if (left>=right)
+            {
+                return;
+            }
+            var pivot = Sorting(left, right);
+            Qsort(left, pivot-1);
+            Qsort(pivot+1, right);
+        }
+        private int Sorting(int left, int right)
+        {
+            var pointer = left;
+            //опорный элемент находится по right - итератором
+            for (int i = left; i <= right; i++)
+            {
+               
+                if (Items[i].CompareTo(Items[right]) == -1)
+                {
+                    Swap(pointer,i);
+                    pointer++;
+                }
+            }
+            Swap(pointer, right);
+            return pointer;
+            
+        }
+    }
+}
---
+++ AlgorithmTests1/QuickSortTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmBase.Tests
+{
+    [TestClass()]
+    public class QuickSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            //arrenge
+            var quick = new QuickSort<int>();
+            var rnd = new Random();
+            var items = new List<int>();
+            for (int i = 0; i < 15; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+            quick.Items.AddRange(items);
+            var sorted = items.OrderBy(x => x).ToArray();//стадартная сортировка List
+            //act
+            quick.Sort();
+            //assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(sorted[i], quick.Items[i]);
+            }
+        }
+    }
+}
